30973f15ad03a8ee613491bcf0736d1f7d83444d
Nicolas Leucci <leucci.nicolas@gmail.com>
2014-05-06 10:31:57 +0200

    Added project Dayoff

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "spyc-master/spyc.php";
+$Data = Spyc::YAMLLoad('test.yml');
+
+var_dump($Data);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>DayOff - When is my next break ?</title>
+<meta charset="utf-8" />
+<meta name="author" content="LEUCCI Nicolas" />
+<link rel="stylesheet" href="css/main.css" />
+<link rel="start" title="Accueil" href="index.php" />
+<link rel="icon" type="image/png" href="img/favicon.ico" />
+</head>
+	<body>
+
+
+
+	<script type="text/javascript" src="js/jquery/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>
