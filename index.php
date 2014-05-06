fc99aaecfcd601a89f494c0381648b836a32d7d9
Nicolas Leucci <leucci.nicolas@gmail.com>
2014-05-06 11:59:07 +0200

    Comparate current date / day off

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once "spyc-master/spyc.php";
-$Data = Spyc::YAMLLoad('Data-day.yml');
+// Initialize Spyc et YAML
+    require_once "spyc-master/spyc.php";
+    $Data = Spyc::YAMLLoad('Data-day.yml');
 
+// Get the current day
+    $CurrentDate = date("Y-m-d");
+    $CurrentDate = strtotime($CurrentDate);
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,7 +20,27 @@ $Data = Spyc::YAMLLoad('Data-day.yml');
 </head>
 	<body>
 
+    <?php
 
+    $i = 0;
+    foreach ($Data as $Day) {
+
+    $Dayoff = date("Y-").$Data[$i]['date'];
+    $Dayoff_DateTime = strtotime($Dayoff);
+
+        if($CurrentDate < $Dayoff_DateTime){
+            echo "<font color=\"green\">".$Dayoff."</font><br />";
+
+        }
+        else{
+            echo "<font color=\"red\">".$Dayoff."</font><br />";
+        }
+
+        $i++;
+    }
+
+
+    ?>
 
 	<script type="text/javascript" src="js/jquery/jquery-1.11.0.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
