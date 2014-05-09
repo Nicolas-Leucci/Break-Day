da8d89595e06f3af01e795c1abeac47c8ddaa37d
Nicolas Leucci <leucci.nicolas@gmail.com>
2014-05-09 11:05:38 +0200

    Wallpaper and design

--- index.php
+++ index.php
@@ -4,21 +4,6 @@
 require_once "spyc-master/spyc.php";
 require_once "Data.php";
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Break-Day - When is my next break ?</title>
-<meta charset="utf-8" />
-<meta name="author" content="LEUCCI Nicolas" />
-<link rel="stylesheet" href="css/main.css" />
-<link rel="start" title="Accueil" href="index.php" />
-<link rel="icon" type="image/png" href="img/favicon.ico" />
-</head>
-	<body>
-
-    <?php
-
 // Get the current day
     $CurrentDate = date("Y-m-d");
     $CurrentDate_str = strtotime($CurrentDate);
@@ -33,8 +18,8 @@ require_once "Data.php";
     $NextWeekend_str = strtotime(date("Y-m-d", strtotime('+'.$tmp.'days')));
 
 // Next weekend by default
-    $Nextdayoff = "The next day off is: ".$NextWeekend."<br />";
-    $Nextdayoff .= "<b>Weekend !</b>";
+    $Nextdayoff = "<p class=\"nextday\">".$NextWeekend."</p>";
+    $Nextdayoff .= "<p class=\"dayname\">Weekend !</p>";
 
 // Is there a day off before weekend ?
     $i = 0;
@@ -46,7 +31,7 @@ require_once "Data.php";
         $Dayoff_DateTime = strtotime($Dayoff);
 
         if($Dayoff_DateTime > $CurrentDate_str && $Dayoff_DateTime < $NextWeekend_str){
-            $Nextdayoff = "The next day off is: ".$Dayoff."<br />";
+            $Nextdayoff = $Dayoff."<br />";
             $Nextdayoff .= "<b>".$Data[$i]['name']."</b>";
             break;
         }
@@ -54,12 +39,37 @@ require_once "Data.php";
         $i++;
     }
 
-    echo $Nextdayoff;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Break-Day - When is my next break ?</title>
+<meta charset="utf-8" />
+<meta name="author" content="LEUCCI Nicolas" />
+<link rel="stylesheet" href="css/main.css" />
+<!--[if lte IE 7]><link rel="stylesheet" href="css/ie7.css" /><![endif]-->
+<link rel="start" title="Accueil" href="index.php" />
+<link rel="icon" type="image/png" href="img/favicon.ico" />
+</head>
+	<body>
+    <div class="outback">
 
+        <div class="innerback">
 
-    ?>
+            <div class="content">
 
+                <p class="date">
+                <?php
+                    echo $Nextdayoff;
+                ?>
+                </p>
+            </div>
+
+        </div>
+
+    </div>
 	<script type="text/javascript" src="js/jquery/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery.backstretch.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,1 @@
+$.backstretch("img/"+(1 + Math.floor(Math.random() * 5))+".png");
